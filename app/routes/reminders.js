af2195a982ca42356a2189a032550a129028fca4
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-04-04 10:29:50 -0600

    Delete comments and model test for reminders

--- app/routes/reminders.js
+++ app/routes/reminders.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    // going to store and trying to find all reminder
     return this.get('store').findAll('reminder');
   }
 });
