3a35cd129a4e949994ca7f145b3bcf9af686d4e0
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-04-05 16:00:16 -0600

    Add semi colons

--- tests/acceptance/edit-test.js
+++ tests/acceptance/edit-test.js
@@ -9,7 +9,7 @@ test('redirects to edit on click', function(assert) {
   server.createList('reminder', 5);
 
   visit('/reminders/1');
-  click('.edit-button')
+  click('.edit-button');
 
   andThen(function() {
     assert.equal(currentURL(), '/reminders/1/edit');
