e26e15fc46baa12c2aab78c6680d6880fa370886
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-04-05 07:03:58 -0600

    Fixed spacing, semi colons, and deleted the log
    Per Taylor's request

--- app/controllers/reminders/new.js
+++ app/controllers/reminders/new.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     select(model) {
-      model.save()
+      model.save();
     }
   }
 });
--- tests/acceptance/new-test.js
+++ tests/acceptance/new-test.js
@@ -10,8 +10,7 @@ moduleForAcceptance('Acceptance | new');
 test('visiting /new', function(assert) {
   server.createList('reminder', 5);
 
-  visit('reminders/new')
-
+  visit('reminders/new');
 
   andThen(function() {
     assert.equal(currentURL(), 'reminders/new');
@@ -22,20 +21,19 @@ test('redirects to new on click', function(assert) {
   server.createList('reminder', 5);
 
   visit('/reminders');
-  click('a:last')
-
+  click('a:last');
 
   andThen(function() {
-    assert.equal(currentURL(), '/reminders/new')
+    assert.equal(currentURL(), '/reminders/new');
   });
 });
 
 test('adding a new reminder', function(assert) {
   server.createList('reminder', 5);
 
-  visit('reminders/new')
-  fillIn('input:first', 'My new post')
-  click('.submit-button')
+  visit('reminders/new');
+  fillIn('input:first', 'My new post');
+  click('.submit-button');
 
   andThen(function() {
     assert.equal(Ember.$('.reminder-item').length, 6);
